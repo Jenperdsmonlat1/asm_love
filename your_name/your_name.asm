23a87896638a46882d4c323ebf6917a9de2da8fe
Cat-Man123 <65119790+Cat-Man123@users.noreply.github.com>
2022-09-01 18:46:21 +0200

    Update your_name.asm

--- your_name/your_name.asm
+++ your_name/your_name.asm
@@ -1,6 +1,6 @@
 SECTION .data
-ask db "Saisissez votre prenom: ", 0Ah
-greeting db "Salut: ", 0Ah
+ask db "Saisissez votre prenom: ", 0Ah, 0h
+greeting db "Salut: ", 0Ah, 0h
 
 SECTION .bss
 name: RESB 255
@@ -9,31 +9,76 @@ SECTION .text
 global _start
 
 _start:
+    mov eax, ask
+    call printf
 
-    mov edx, 24
-    mov ecx, ask
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    mov ebx, name
+    call scanf
+
+    mov eax, greeting
+    call printf
+
+    mov eax, name
+    call printf
+
+    call exit
+
+strlen:
+    push ebx
+    mov ebx, eax
+
+nxchar:
+    cmp byte [eax], 0
+    jz length
+    inc eax
+    jmp nxchar
+
+length:
+    sub eax, ebx
+    pop ebx
+    ret
+
+scanf:
+    push edx
+    push ecx
+    push ebx
+    push eax
 
     mov edx, 255
-    mov ecx, name
+    mov ecx, ebx
+    pop ebx
+
     mov ebx, 0
     mov eax, 3
     int 0x80
 
-    mov edx, 7
-    mov ecx, greeting
+    pop edx
+    pop ecx
+    pop eax
+    ret
+
+printf:
+    push edx
+    push ecx
+    push ebx
+    push eax
+    call strlen
+
+    mov edx, eax
+    pop eax
+
+    mov ecx, eax
     mov ebx, 1
     mov eax, 4
     int 0x80
 
-    mov edx, 255
-    mov ecx, name
-    mov ebx, 1
-    mov eax, 4
-    int 0x80
+    pop edx
+    pop ecx
+    pop ebx
+    ret
 
-    mov ebx, 0
+exit:
+    xor ebx, ebx
     mov eax, 1
     int 0x80
+    ret
